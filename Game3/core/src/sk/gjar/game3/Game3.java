922d2f8ecf8eaca8542ea54ce563be8ab808b816
Jan Lenko <lenkojan@gmail.com>
2018-10-15 20:09:37 +0200

    Added triangle platform solution

--- Game3/core/src/sk/gjar/game3/Game3.java
+++ Game3/core/src/sk/gjar/game3/Game3.java
@@ -14,22 +14,24 @@ public class Game3 extends ApplicationAdapter {
     Texture platformRight;
     static final int WIDTH = 10;
     static final int TEXTURE_WIDTH = 128;
+    static final int TEXTURE_HEIGHT = 128;
     /* solution 1
     Texture[] bottomLine;
     */
-    /* solution 2
+   /*
     int[] bottomLine;
     */
-    /* solution 3
-    int[] bottomLine;
+
+    int[] bottomLine1;
+    int[] bottomLine2;
+    int[] bottomLine3;
     Texture[] textures;
-    */
-    /* zombie texture
+
+
     Texture zombieTexture;
-    */
-    /* zombie sprite
+
     Sprite zombieSprite;
-    */
+
 
 
     @Override
@@ -38,14 +40,14 @@ public class Game3 extends ApplicationAdapter {
         platformLeft = new Texture("1.png");
         platformCenter = new Texture("2.png");
         platformRight = new Texture("3.png");
-        /* zombie texture
+
         zombieTexture = new Texture("Idle (1).png");
-        */
-        /* zombie sprite
+
+
         zombieSprite = new Sprite(zombieTexture);
         zombieSprite.setScale(0.3f);
         zombieSprite.setPosition(400, -60);
-        */
+
         /* solution 1
         bottomLine = new Texture[WIDTH];
         bottomLine[0] = null;
@@ -64,16 +66,17 @@ public class Game3 extends ApplicationAdapter {
         bottomLine = new int[]{ 0, 1, 2, 2,2,2, 2, 2, 3, 0};
         */
 
-        /* solution 3
         textures = new Texture[]{platformLeft, platformCenter, platformRight};
-        bottomLine = new int[]{0, 0, 1, 2, 2, 2, 2, 3, 0, 0};
-        */
+        bottomLine1 = new int[]{0, 0, 1, 2, 2, 2, 2, 3, 0, 0};
+        bottomLine2 = new int[]{0, 0, 0, 1, 2, 2, 3, 0, 0, 0};
+        bottomLine3 = new int[]{0, 0, 0, 0, 1, 3, 0, 0, 0, 0};
+
 
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(135f/255f, 206f/255f, 235f/255f, 1);
+        Gdx.gl.glClearColor(135f / 255f, 206f / 255f, 235f / 255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
 
@@ -105,23 +108,38 @@ public class Game3 extends ApplicationAdapter {
         }
         */
 
-        /* solution 3
-        for (int i = 0; i < bottomLine.length; i++) {
-            if (bottomLine[i] > 0) {
-                batch.draw(textures[bottomLine[i] - 1],
+
+        for (int i = 0; i < bottomLine1.length; i++) {
+            if (bottomLine1[i] > 0) {
+                batch.draw(textures[bottomLine1[i] - 1],
                         i * TEXTURE_WIDTH,
                         0);
             }
         }
-        */
+
+        for (int i = 0; i < bottomLine2.length; i++) {
+            if (bottomLine2[i] > 0) {
+                batch.draw(textures[bottomLine2[i] - 1],
+                        i * TEXTURE_WIDTH,
+                        1 * TEXTURE_HEIGHT);
+            }
+        }
+
+        for (int i = 0; i < bottomLine3.length; i++) {
+            if (bottomLine3[i] > 0) {
+                batch.draw(textures[bottomLine3[i] - 1],
+                        i * TEXTURE_WIDTH,
+                        2 * TEXTURE_HEIGHT);
+            }
+        }
 
         /* zombie texture
         batch.draw(zombieTexture, 400, 110);
         */
 
-        /* zombie sprite
+
         zombieSprite.draw(batch);
-        */
+
         batch.end();
     }
 
